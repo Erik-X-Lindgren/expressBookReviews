4ab8ffbbdae732dcfc7bd4dde56695fcfb7dc626
eriklindgren1 <erik.lindgren@aller.com>
2024-08-24 13:16:33 +0200

    Finished project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,56 +51,121 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+//  Task10
+// Get book lists
+const getBooks = () => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+};
+
+//  Get the book list available in the shop
+public_users.get('/',async function (req, res) {
+  try {
+    const bookList = await getBooks(); 
+    res.json(bookList); // Neatly format JSON output
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
+
+// Task 1
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    // Send JSON response with formatted books data
-    res.send(JSON.stringify(books,null,4));
-});
+//public_users.get('/',function (req, res) {
+//    // Send JSON response with formatted books data
+//    res.send(JSON.stringify(books,null,4));
+//});
 
+//  Task 11
 // Get book details based on ISBN
+const getByISBN = (isbn) => {
+    return new Promise((resolve, reject) => {
+        let isbnNum = parseInt(isbn);
+        if (books[isbnNum]) {
+            resolve(books[isbnNum]);
+        } else {
+            reject({ status: 404, message: `ISBN ${isbn} not found` });
+        }
+    });
+};
+
+//  Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  // Retrieve the isbn parameter from the request URL and send the corresponding books's details
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    getByISBN(req.params.isbn)
+    .then(
+        result => res.send(result),
+        error => res.status(error.status).json({message: error.message})
+    );
  });
-  
-// Get book details based on author
+
+// Task 2
+// Get book details based on ISBN
+//public_users.get('/isbn/:isbn',function (req, res) {
+//  // Retrieve the isbn parameter from the request URL and send the corresponding books's details
+//    const isbn = req.params.isbn;
+//    res.send(books[isbn]);
+// });
+
+// Task 12
+//  Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  const bookDetails = [];
-
-  // Iterate through the books object
-  for (let key in books) {
-    if (books[key].author === author) {
-      bookDetails.push(books[key]);
-    }
-  }
-
-  if (bookDetails.length > 0) {
-    return res.status(200).json(bookDetails);
-  } else {
-    return res.status(404).json({message: "No books found for the given author"});
-  }
+    const author = req.params.author;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.author === author))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
-// Get all books based on title
+// Task 3
+// Get book details based on author
+//public_users.get('/author/:author',function (req, res) {
+//  const author = req.params.author;
+//  const bookDetails = [];
+//
+//  // Iterate through the books object
+//  for (let key in books) {
+//    if (books[key].author === author) {
+//      bookDetails.push(books[key]);
+//    }
+//  }
+//
+//  if (bookDetails.length > 0) {
+//    return res.status(200).json(bookDetails);
+//  } else {
+//    return res.status(404).json({message: "No books found for the given author"});
+//  }
+//});
+
+// Task 13
+//  Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  const bookDetails = [];
-
-  // Iterate through the books object
-  for (let key in books) {
-    if (books[key].title === title) {
-      bookDetails.push(books[key]);
-    }
-  }
-
-  if (bookDetails.length > 0) {
-    return res.status(200).json(bookDetails);
-  } else {
-    return res.status(404).json({message: "No books found for the given author"});
-  }
+    const title = req.params.title;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.title === title))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
+
+// Task 4
+// Get all books based on title
+//public_users.get('/title/:title',function (req, res) {
+//  const title = req.params.title;
+//  const bookDetails = [];
+//
+//  // Iterate through the books object
+//  for (let key in books) {
+//    if (books[key].title === title) {
+//      bookDetails.push(books[key]);
+//    }
+//  }
+//
+//  if (bookDetails.length > 0) {
+//    return res.status(200).json(bookDetails);
+//  } else {
+//    return res.status(404).json({message: "No books found for the given author"});
+//  }
+//});
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
